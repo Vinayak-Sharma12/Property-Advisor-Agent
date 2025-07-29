903858a84b891fbc266fb3b7f799bb2e4f3b99f7
Vinayak Sharma <svinayak580@gmail.com>
2025-07-29 23:46:29 +0530

    changed model name

--- intent_detection_agent.py
+++ intent_detection_agent.py
@@ -1,13 +1,13 @@
-from llm_models import llama_model
+from llm_models import deepseek_model
 from parser_and_prompts import intent_prompt
 from parser_and_prompts import parser
 
 def find_intent(user_query):
-    intent_chain=intent_prompt|llama_model|parser
+    intent_chain=intent_prompt|deepseek_model|parser
     intent=intent_chain.invoke({'user_query':user_query})
     return intent
 
-# print(find_intent(input("Enter the query")))
+print(find_intent(input("Enter the query")))
 
      
      
--- llm_models.py
+++ llm_models.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-llama_model = ChatGroq(
+deepseek_model = ChatGroq(
     model="deepseek-r1-distill-llama-70b",  
     api_key=os.getenv("GROQ_API_KEY")
 )
